33e7f2976f67d552e316b3a18c2eb7468fa72566
AbtahiHasan <anikidpersonal@gmail.com>
2023-04-21 21:55:38 +0600

    text slide done

--- src/components/TopHeader.jsx
+++ src/components/TopHeader.jsx
@@ -1,5 +1,6 @@
 import moment from "moment";
 import logo from "/logo.png"
+import Marquee from "react-fast-marquee";
 
 const TopHeader = () => {
     return (
@@ -7,6 +8,12 @@ const TopHeader = () => {
             <img src={logo} className="h-[40px] mx-auto" alt="logo" />
             <p className="text-center mt-4">Journalism Without Fear or Favour</p>
             <p className="font-bold text-center mt-[10px]">{moment().format("dddd, MMMM D, YYYY")}</p>
+            <div className="bg-[#F3F3F3] p-2 mt-6 flex items-center">
+                <button className="px-4 py-2 text-white rounded bg-[#D72050]">Latest</button>
+                <Marquee>
+                    <p>Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</p>
+                </Marquee>
+            </div>
         </header>
     );
 };
