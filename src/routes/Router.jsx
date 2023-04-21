0ecaf3e7857e5c3a47e67a0b5e9341b28c4363da
AbtahiHasan <anikidpersonal@gmail.com>
2023-04-21 17:26:34 +0600

    dragon-news basic setup done

---
+++ src/routes/Router.jsx
@@ -0,0 +1,51 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import LoginLayout from "../layouts/LoginLayout";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import MainLayout from "../layouts/MainLayout";
+import Categories from "../components/Categories";
+import NewsLayout from "../layouts/NewsLayout";
+import News from "../components/News";
+
+const Router = createBrowserRouter([
+    {
+        path: "/",
+        element: <LoginLayout/>,
+        children: [
+            {
+                path: "/",
+                element: <Navigate to="/category/0"/>
+            },
+            {
+                path: "/login",
+                element: <Login/>
+            },
+            {
+                path: "/register",
+                element: <Register/>
+            }
+        ]
+    },
+    {
+        path: "/category",
+        element: <MainLayout/>,
+        children: [
+            {
+                path: ":id",
+                element: <Categories/>
+            }
+        ]
+    },
+    {
+        path: "/news",
+        element: <NewsLayout/>,
+        children: [
+            {
+                path: ":id",
+                element: <News/>
+            }
+        ]
+    }
+])
+
+export default Router
