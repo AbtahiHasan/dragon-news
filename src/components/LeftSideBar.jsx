84a842566867f322326469dee94f515f4a9ffce4
AbtahiHasan <anikidpersonal@gmail.com>
2023-04-28 12:33:37 +0600

    left side card or editor card added

--- src/components/LeftSideBar.jsx
+++ src/components/LeftSideBar.jsx
@@ -1,5 +1,9 @@
 import { NavLink } from "react-router-dom";
 import { useApiData } from "../context/ApiDataProvider";
+import img1 from "/assets/1.png"
+import img2 from "/assets/2.png"
+import img3 from "/assets/3.png"
+import { BsCalendar } from "react-icons/bs";
 
 
 const LeftSideBar = () => {
@@ -12,6 +16,36 @@ const LeftSideBar = () => {
                     categories.map((category) => <li className="font-bold " key={category.id}><NavLink to={`/categories/${category.id}`}  className={({isActive}) => isActive ? "bg-[#E7E7E7] w-full rounded block pl-7 py-3" : "pl-7 block py-3"}>{category.name}</NavLink></li>)
                 }
             </ul>
+
+            <section className="mt-8">
+                <article className="mt-8">
+                    <img className="w-full" src={img1} alt="" />
+                    <h3 className="text-lg font-bold my-3">Bayern Slams Authorities Over Flight Delay to Club World Cup</h3>
+                    <div className="flex items-center gap-2">
+                        <p>Sports</p>
+                        <BsCalendar/>
+                        <p>Jan 4, 2022</p>
+                    </div>
+                </article>
+                <article className="mt-8">
+                    <img className="w-full" src={img2} alt="" />
+                    <h3 className="text-lg font-bold my-3">Bayern Slams Authorities Over Flight Delay to Club World Cup</h3>
+                    <div className="flex items-center gap-2">
+                        <p>Sports</p>
+                        <BsCalendar/>
+                        <p>Jan 4, 2022</p>
+                    </div>
+                </article>
+                <article className="mt-8">
+                    <img className="w-full" src={img3} alt="" />
+                    <h3 className="text-lg font-bold my-3">Bayern Slams Authorities Over Flight Delay to Club World Cup</h3>
+                    <div className="flex items-center gap-2">
+                        <p>Sports</p>
+                        <BsCalendar/>
+                        <p>Jan 4, 2022</p>
+                    </div>
+                </article>
+            </section>
         </aside>
     );
 };
--- src/pages/NewsDetails.jsx
+++ src/pages/NewsDetails.jsx
@@ -3,7 +3,11 @@ import TopHeader from '../components/TopHeader';
 import RightSidebar from '../components/RightSidebar';
 import Container from '../components/Container';
 import { Link, useParams } from 'react-router-dom';
-import { BsArrowLeft } from "react-icons/bs";
+import { BsArrowLeft, BsCalendar} from "react-icons/bs";
+import img1 from "/assets/1.png"
+import img2 from "/assets/2.png"
+import img3 from "/assets/3.png"
+
 
 const NewsDetails = () => {
     const {id} = useParams()
@@ -25,6 +29,36 @@ const NewsDetails = () => {
                         <h2 className='text-xl font-bold my-3'>{title}</h2>
                         <p>{details}</p>
                         <Link className='px-4 py-2 text-white rounded bg-[#D72050] mt-8 inline-block ' to={`/categories/${category_id}`}><div className='flex items-center gap-2'><BsArrowLeft/> <span>All news in this category</span></div></Link>
+                        <h2 className='mt-8'>Editors Insight</h2>
+                        <section className='grid md:grid-cols-3 gap-5 mb-8'>
+                            <article className="mt-8">
+                                  <img className="w-full" src={img1} alt="" />
+                                  <h3 className="text-lg font-bold my-3">Bayern Slams Authorities Over Flight Delay to Club World Cup</h3>
+                                  <div className="flex items-center gap-2">
+                                      <p>Sports</p>
+                                      <BsCalendar/>
+                                      <p>Jan 4, 2022</p>
+                                  </div>
+                              </article>
+                              <article className="mt-8">
+                                  <img className="w-full" src={img2} alt="" />
+                                  <h3 className="text-lg font-bold my-3">Bayern Slams Authorities Over Flight Delay to Club World Cup</h3>
+                                  <div className="flex items-center gap-2">
+                                      <p>Sports</p>
+                                      <BsCalendar/>
+                                      <p>Jan 4, 2022</p>
+                                  </div>
+                              </article>
+                              <article className="mt-8">
+                                  <img className="w-full" src={img3} alt="" />
+                                  <h3 className="text-lg font-bold my-3">Bayern Slams Authorities Over Flight Delay to Club World Cup</h3>
+                                  <div className="flex items-center gap-2">
+                                      <p>Sports</p>
+                                      <BsCalendar/>
+                                      <p>Jan 4, 2022</p>
+                                  </div>
+                              </article>
+                        </section>
                     </div>
                 </div>
                 <div><RightSidebar/></div>
