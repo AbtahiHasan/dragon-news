32614e911927c4dafd81e22357fdcc26b4f67388
AbtahiHasan <anikidpersonal@gmail.com>
2023-04-28 10:58:40 +0600

    right side banner added

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -4,6 +4,8 @@ import { FaFacebook } from "react-icons/fa";
 import qzone1 from "/assets/qZone1.png"
 import qzone2 from "/assets/qZone2.png"
 import qzone3 from "/assets/qZone3.png"
+import bg from "/assets/bg.png"
+import { Link } from "react-router-dom";
 
 const RightSidebar = () => {
     return (
@@ -42,7 +44,14 @@ const RightSidebar = () => {
                 <img src={qzone3} alt="" />
             </div>
 
-
+            <div className="w-full h-[50vh] mt-8 relative">
+                <img className="w-full h-full" src={bg} alt="" />
+                <div className="absolute top-0 flex justify-center items-center flex-col h-full p-5 text-white text-center">
+                    <h2 className="text-[30px] font-bold">Create an Amazing Newspaper</h2>
+                    <p className="my-4">Discover thousands of options, easy to customize layouts, one-click to import demo and much more.</p>
+                    <Link to="/" className="px-4 py-2 text-white rounded bg-[#D72050]">Learn More</Link>
+                </div>
+            </div>
 
         </div>
     );
