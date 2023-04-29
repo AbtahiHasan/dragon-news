3a6334837d1d55dbd05321a1c21f664a6f8608ad
AbtahiHasan <anikidpersonal@gmail.com>
2023-04-29 23:16:20 +0600

    login register page added

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import Router from './routes/Router'
 import { ApiDataProvider } from './context/ApiDataProvider'
+import { AuthProvider } from './context/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ApiDataProvider>
-      <RouterProvider router={Router}/>
-    </ApiDataProvider>
+    <AuthProvider>
+      <ApiDataProvider>
+        <RouterProvider router={Router}/>
+      </ApiDataProvider>
+    </AuthProvider>
+    
   </React.StrictMode>,
 )
